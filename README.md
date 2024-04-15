2ddb620848c0498e83346e585f2a6abebf150431
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-15 09:14:31 -0300

    150

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Hundred Days of Python
 
 An Udemy course made by Dr. Angela Yu
+
+# Setup
+
+1. Clone the git repository;
+
+2. Setup a virtual environment (optional;)
+
+3. '''pip install -r requirements.txt'''
+
+4. Run the code!
+
+# What could go wrong
+
+1. Before running the code you may have to select your Python interpreter manually.
+And if you're using a virtual environment you may have to select it as your interpreter.
+In this example we used VScode as our IDE, which let you select an interpreter by clicking a non-working import, for example.
--- day-16/main.py
+++ day-16/main.py
@@ -1,18 +1,24 @@
 # import another_module
 # import turtle
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
 
+# Basics
 # print(another_module.another_variable)
 
 # timmy = turtle.Turtle()
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
 
-timmy.forward(100)
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# Table
+table = PrettyTable()
+print(table)
