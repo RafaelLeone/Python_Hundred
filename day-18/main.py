af7cc48620835bb6c411566e1e048cbd7c3117e8
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-15 17:41:23 -0300

    175

--- day-18/main.py
+++ day-18/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from random import randrange, choice
+import colorgram
 
 
 timmy_the_turtle = Turtle()
@@ -43,13 +44,54 @@ screen.colormode(255)
 #     timmy_the_turtle.right(choice(choices))
 
 # CHALLENGE 5
+# timmy_the_turtle.speed("fastest")
+# for i in range(120):
+#     r = randrange(256)
+#     g = randrange(256)
+#     b = randrange(256)
+#     timmy_the_turtle.pencolor((r, g, b))
+#     timmy_the_turtle.circle(100)
+#     timmy_the_turtle.right(3)
+
+# Hirst Challenge
+CIRCLE_SIZE = 5
+GAP = 50
+colors = colorgram.extract('images/kirb.png', 10)
+pallet = []
+for color in colors:
+    r = color.rgb.g
+    g = color.rgb.b
+    b = color.rgb.r
+    pallet.append((r, g, b))
+pallet.pop(0)
+timmy_the_turtle.width(10)
 timmy_the_turtle.speed("fastest")
-for i in range(120):
-    r = randrange(256)
-    g = randrange(256)
-    b = randrange(256)
-    timmy_the_turtle.pencolor((r, g, b))
-    timmy_the_turtle.circle(100)
-    timmy_the_turtle.right(3)
+timmy_the_turtle.hideturtle()
+y = 300
+
+def draw_circles(pallet):
+    for i in range(9):
+        color = choice(pallet)
+        timmy_the_turtle.pencolor(color)
+        timmy_the_turtle.pendown()
+        timmy_the_turtle.circle(CIRCLE_SIZE)
+        timmy_the_turtle.penup()
+        timmy_the_turtle.forward(GAP)
+
+def get_down(y):
+    y -= GAP
+    timmy_the_turtle.sety(y)
+    return y
+
+for i in range(10):
+    timmy_the_turtle.penup()
+    timmy_the_turtle.setx(-220)
+    y = get_down(y)
+    draw_circles(pallet)
+    color = choice(pallet)
+    timmy_the_turtle.pencolor(color)
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.circle(CIRCLE_SIZE)
+    timmy_the_turtle.penup()
 
 screen.exitonclick()
