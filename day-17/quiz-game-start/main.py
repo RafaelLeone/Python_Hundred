b41895d2ab5c0519a143bdeca35fb43cefec9fb8
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-15 11:34:21 -0300

    162

--- day-17/quiz-game-start/main.py
+++ day-17/quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 question_bank = []
@@ -9,3 +10,11 @@ for element in question_data:
     question_bank.append(Question(element["text"], element["answer"]))
 
 # print(question_bank[0].text)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the Quiz!")
+print(f"Your final score was {quiz.score} out of {quiz.question_number}!")
