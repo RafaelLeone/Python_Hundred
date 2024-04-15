6c177d056c30491867a88ef57ee2c45fdb090cd1
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-15 16:17:52 -0300

    173

--- day-18/main.py
+++ day-18/main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-from random import randrange
+from random import randrange, choice
 
 
 timmy_the_turtle = Turtle()
@@ -8,25 +8,48 @@ timmy_the_turtle.shape("turtle")
 screen = Screen()
 screen.colormode(255)
 
+# CHALLENGE 2
 # for i in range(25):
 #     timmy_the_turtle.pendown()
 #     timmy_the_turtle.forward(10)
 #     timmy_the_turtle.penup()
 #     timmy_the_turtle.forward(10)
 
-CIRCLE = 360
-angle = 120
-sides = 3
+# CHALLENGE 3
+# CIRCLE = 360
+# angle = 120
+# sides = 3
+# while angle >= 36:
+#     r = randrange(256)
+#     g = randrange(256)
+#     b = randrange(256)
+#     timmy_the_turtle.pencolor((r, g, b))
+#     for i in range(sides):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(angle)
+#     sides += 1
+#     angle = CIRCLE/sides
 
-while angle >= 36:
+# CHALLENGE 4
+# choices = [0, 90, 270]
+# timmy_the_turtle.speed("fast")
+# for i in range(100):
+#     r = randrange(256)
+#     g = randrange(256)
+#     b = randrange(256)
+#     timmy_the_turtle.pencolor((r, g, b))
+#     timmy_the_turtle.width(5)
+#     timmy_the_turtle.forward(20)
+#     timmy_the_turtle.right(choice(choices))
+
+# CHALLENGE 5
+timmy_the_turtle.speed("fastest")
+for i in range(120):
     r = randrange(256)
     g = randrange(256)
     b = randrange(256)
     timmy_the_turtle.pencolor((r, g, b))
-    for i in range(sides):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(angle)
-    sides += 1
-    angle = CIRCLE/sides
+    timmy_the_turtle.circle(100)
+    timmy_the_turtle.right(3)
 
 screen.exitonclick()
