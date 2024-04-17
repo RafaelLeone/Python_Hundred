125067b86980903817420c0ed473d13ee88343e3
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-17 12:02:38 -0300

    186

--- day-20-21-snake-game/main.py
+++ day-20-21-snake-game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time
 
 
 screen = Screen()
@@ -6,11 +7,13 @@ screen.colormode(255)
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snaky")
+screen.tracer(0)
 
 # Global constants and variables
 BLOCK_SIZE = 20
 current_position = 0
 snake_blocks = {}
+game = True
 
 for i in range(3):
     snake_blocks['turtle_' + str(i)] = Turtle()
@@ -19,5 +22,11 @@ for i in range(3):
     snake_blocks['turtle_' + str(i)].penup()
     snake_blocks['turtle_' + str(i)].setx(current_position)
     current_position -= BLOCK_SIZE
+
+while game:
+    screen.update()
+    time.sleep(0.1)
+    for snake_block in snake_blocks:
+        snake_blocks[snake_block].forward(20) 
 
 screen.exitonclick()
