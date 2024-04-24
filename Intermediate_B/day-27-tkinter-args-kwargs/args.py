b6f6727ad442a93f893a81cfed01c3b78fcbe750
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-24 16:05:00 -0300

    251

--- Intermediate_B/day-27-tkinter-args-kwargs/args.py
+++ Intermediate_B/day-27-tkinter-args-kwargs/args.py
@@ -8,3 +8,28 @@ def add(*args):
 
 print(add())
 print(add(3, 3, 5, 10))
+
+def calculate(**kwargs):
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+    print(kwargs["add"])
+
+calculate(add=1, multiply=2)
+
+def deep_calculate(n, **kwargs):
+    n += kwargs["add"]
+    print(n)
+
+deep_calculate(2, add=1, multiply=2)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan")
+print(my_car.model)
