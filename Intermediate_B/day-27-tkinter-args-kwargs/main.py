0f98b5e5b3063ced3f7e1d7fe96cc2f4ac0ac57a
RafaelLeone <rinaciodacosta@gmail.com>
2024-04-24 16:39:02 -0300

    254

--- Intermediate_B/day-27-tkinter-args-kwargs/main.py
+++ Intermediate_B/day-27-tkinter-args-kwargs/main.py
@@ -5,18 +5,19 @@ import tkinter
 window = tkinter.Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label.
 my_label = tkinter.Label(text="I am Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(row=0, column=0)
 # my_label.pack(side = "left")
 
 my_label["text"] = "new"
-my_label.config(text = "New Text")
+my_label.config(text = "New Text", padx=20, pady=20)
 
 # Entry.
-input = tkinter.Entry(width=50)
-input.pack()
+input = tkinter.Entry(width=20)
+input.grid(row=1, column=1)
 
 # Button.
 def button_clicked():
@@ -25,6 +26,6 @@ def button_clicked():
     print("I got clicked.")
 
 button = tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+button.grid(row=2, column=2)
 
 window.mainloop()
