37b3c4ff28cdcd98644b0eea75b6df69252b5da5
RafaelLeone <rinaciodacosta@gmail.com>
2024-05-01 17:25:16 -0300

    326

---
+++ Intermediate_+/day-37-get-post-put-delete/main.py
@@ -0,0 +1,34 @@
+import requests
+
+
+USERNAME = "rafaelleone"
+TOKEN = "hf948923hr94w30gf0e00wa8"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph000",
+    "name": "Workout",
+    "unit": "day",
+    "type": "int",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
