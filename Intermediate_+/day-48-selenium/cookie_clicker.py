286a8967ddba8dc3b4135b6804b1ccd8de54ea6d
RafaelLeone <rinaciodacosta@gmail.com>
2024-05-03 11:39:47 -0300

    cookie done

--- Intermediate_+/day-48-selenium/cookie_clicker.py
+++ Intermediate_+/day-48-selenium/cookie_clicker.py
@@ -1,46 +1,52 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 
 # Keep Chrome browser open.
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome(options=chrome_options)
-
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+INTERVAL = 0.5 # Change for testing.
 last_check_time = time.time()
-INTERVAL = 0.5
+numbers = "0123456789"
 
 cookie = driver.find_element(By.ID, value="cookie")
-money = driver.find_element(By.ID, value="money")
+list_of_upgrades = ["Cursor", "Grandma", "Factory", "Mine", "Shipment", "Alchemy lab", "Portal", "Time machine"]
+amount = 0
+i = 0
 
-buy_cursor = driver.find_element(By.ID, value="buyCursor")
-buy_cursor_number = [letter for letter in buy_cursor.text[:29] if letter in numbers]
-buy_cursor_price = "".join(buy_cursor_number)
-cursor_amount = 0
+UPGRADE_LIMIT = 1 # Change for testing.
+
+def buy_upgrade(upgrade):
+    value = f"buy{upgrade}"
+    element = driver.find_element(By.ID, value=value)
+    buy_number = [char for char in element.text if char in numbers]
+    if "Autoclicks every 5 seconds." in element.text:
+        buy_number.pop()
+    buy_price = "".join(buy_number)
+    return element, buy_price
 
 while True:
     current_time = time.time()
     if current_time - last_check_time >= INTERVAL:
-        last_check_time = current_time
-        money = driver.find_element(By.ID, value="money")
-        buy_cursor = driver.find_element(By.ID, value="buyCursor")
-
-        buy_cursor_number = [letter for letter in buy_cursor.text[:29] if letter in numbers]
-        buy_cursor_price = "".join(buy_cursor_number)
-        if cursor_amount >= 99:
-            break # TO DO: add other upgrades instead of breaking.
-        elif int(money.text) >= int(buy_cursor_price):
-            buy_cursor.click()
-            cursor_amount += 1
+        if amount < UPGRADE_LIMIT:
+            last_check_time = current_time
+            money = driver.find_element(By.ID, value="money")
+            element, buy_price = buy_upgrade(list_of_upgrades[i])
+            if int((money.text).replace(",", "")) >= int(buy_price):
+                element.click()
+                amount += 1
+        elif i< len(list_of_upgrades):
+            i += 1
+            amount = 0
+        else:
+            break
     cookie.click()
     time.sleep(0.01)
 
 time.sleep(5)
 print("Success")
-driver.quit()
+# driver.quit()
